1e4791a422a59110f3ffa5592341f8400496ed57
eeris43 <92627529+eeris43@users.noreply.github.com>
2023-06-01 22:26:56 +0300

    Dosya Düzenleme

--- KodAnaliz_FileZip/Form1.cs
+++ KodAnaliz_FileZip/Form1.cs
@@ -10,16 +10,38 @@ namespace KodAnaliz_FileZip
         public Form1()
         {
             InitializeComponent();
+            string zipPath = @".\result.zip";
+
+            Console.WriteLine("Provide path where to extract the zip file:");
+            string extractPath = Console.ReadLine();
+
+            // Normalizes the path.
+            extractPath = Path.GetFullPath(extractPath);
+
+            // Ensures that the last character on the extraction path
+            // is the directory separator char.
+            // Without this, a malicious zip file could try to traverse outside of the expected
+            // extraction path.
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                extractPath += Path.DirectorySeparatorChar;
+
+            ZipArchive archive = ZipFile.OpenRead(zipPath);
+
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                DosyaAyikla(entry);
+            }
+
         }
 
         private static string HedefDosya = "/örnek/Tetser/";
 
-        public void DosyaAyikla(IEnumerator<ZipArchiveEntry> SikistilmisDosya)
+        public void DosyaAyikla(ZipArchiveEntry SikistilmisDosya)
         {
-            ZipArchiveEntry entry = SikistilmisDosya.Current;
-            string destinationPath = Path.Combine(HedefDosya, entry.FullName);
+            
+            string destinationPath = Path.Combine(HedefDosya, SikistilmisDosya.FullName);
 
-            entry.ExtractToFile(destinationPath);
+            SikistilmisDosya.ExtractToFile(destinationPath);
         }
     }
 
